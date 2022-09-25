3c813226cb91b083d6979882b03e4c3c822133b8
Donaldoo <donald.kumaraku@gmail.com>
2022-09-25 11:44:39 -0700

    Added test file

--- deck_of_cards/deck_of_cards.py
+++ deck_of_cards/deck_of_cards.py
@@ -109,32 +109,3 @@ class Player:
         total = sum(points)
         print("{} points".format(total))
         return total
-
-
-
-"""Tests"""
-deck = Deck()
-deck.shuffle()
-
-bob = Player("Bob", 50)
-ben = Player("Ben", 51)
-
-for i in range(5):
-    bob.draw(deck)
-    ben.draw(deck)
-
-print("Bob:")
-bob.showHand()
-bob_points = bob.evaluate_points()
-
-print("\nBen:")
-ben.showHand()
-ben_points = ben.evaluate_points()
-
-if bob_points > ben_points:
-    print("\nBob wins")
-elif ben_points > bob_points:
-    print("\nBen wins")
-else:
-    print("\nDraw")
-
